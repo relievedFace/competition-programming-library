da8844dc62b2692579283aa0635b6a54f3fc13a8
relievedFace <relievedFace@gmail.com>
2023-06-27 06:05:24 +0900

    Add shift matrix function.

--- src/matrix.rs
+++ src/matrix.rs
@@ -15,3 +15,71 @@ where
         .map(|chunk| chunk.collect())
         .collect()
 }
+
+pub fn shift_right<T>(v: &[Vec<T>], shift: usize) -> Vec<Vec<T>>
+where
+    T: Clone + Default,
+{
+    let h = v.len();
+    let w = v[0].len();
+    let mut shifted_v = vec![vec![T::default(); w]; h];
+
+    for i in 0..h {
+        for j in 0..w {
+            shifted_v[i][(j + shift) % w] = v[i][j].clone();
+        }
+    }
+
+    shifted_v
+}
+
+pub fn shift_left<T>(v: &[Vec<T>], shift: usize) -> Vec<Vec<T>>
+where
+    T: Clone + Default,
+{
+    let h = v.len();
+    let w = v[0].len();
+    let mut shifted_v = vec![vec![T::default(); w]; h];
+
+    for i in 0..h {
+        for j in 0..w {
+            shifted_v[i][(w + j - shift) % w] = v[i][j].clone();
+        }
+    }
+
+    shifted_v
+}
+
+pub fn shift_down<T>(v: &[Vec<T>], shift: usize) -> Vec<Vec<T>>
+where
+    T: Clone + Default,
+{
+    let h = v.len();
+    let w = v[0].len();
+    let mut shifted_v = vec![vec![T::default(); w]; h];
+
+    for i in 0..h {
+        for j in 0..w {
+            shifted_v[(i + shift) % h][j] = v[i][j].clone();
+        }
+    }
+
+    shifted_v
+}
+
+pub fn shift_up<T>(v: &[Vec<T>], shift: usize) -> Vec<Vec<T>>
+where
+    T: Clone + Default,
+{
+    let h = v.len();
+    let w = v[0].len();
+    let mut shifted_v = vec![vec![T::default(); w]; h];
+
+    for i in 0..h {
+        for j in 0..w {
+            shifted_v[(h + i - shift) % h][j] = v[i][j].clone();
+        }
+    }
+
+    shifted_v
+}
