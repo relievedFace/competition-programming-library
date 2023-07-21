7e8b204f7cad902e5b761618301795440b1ef7e8
relievedFace <relievedFace@gmail.com>
2023-07-22 00:43:08 +0900

    fix

--- src/union_find.rs
+++ src/union_find.rs
@@ -41,8 +41,65 @@ impl UnionFind {
         self.root(x) == self.root(y)
     }
 
-    pub fn size(&mut self, x: usize) -> usize {
+    pub fn size(&self, x: usize) -> usize {
+        self.sizes[x]
+    }
+
+    pub fn root_size(&mut self, x: usize) -> usize {
         let root = self.root(x);
         self.sizes[root]
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_uf() {
+        //    0           3
+        //  /   \       /   \
+        // 1     2     4     6
+        //           / | \   |
+        //          5  7  8  9
+        let mut uf = UnionFind::new(10);
+        uf.unite(0, 1);
+        uf.unite(0, 2);
+        uf.unite(3, 4);
+        uf.unite(3, 6);
+        uf.unite(4, 5);
+        uf.unite(4, 7);
+        uf.unite(4, 8);
+        uf.unite(6, 9);
+
+        // test root
+        assert_eq!(0, uf.root(0));
+        assert_eq!(0, uf.root(1));
+        assert_eq!(0, uf.root(2));
+        assert_eq!(3, uf.root(3));
+        assert_eq!(3, uf.root(4));
+        assert_eq!(3, uf.root(5));
+        assert_eq!(3, uf.root(6));
+        assert_eq!(3, uf.root(7));
+        assert_eq!(3, uf.root(8));
+        assert_eq!(3, uf.root(9));
+
+        // test same
+        assert!(uf.same(0, 0));
+        assert!(uf.same(0, 1));
+        assert!(uf.same(0, 2));
+        assert!(uf.same(1, 0));
+        assert!(uf.same(1, 1));
+        assert!(uf.same(1, 2));
+        assert!(uf.same(2, 0));
+        assert!(uf.same(2, 1));
+        assert!(uf.same(2, 2));
+
+        assert!(!uf.same(0, 3));
+        assert!(!uf.same(1, 3));
+        assert!(!uf.same(2, 8));
+
+        assert!(uf.same(3, 8));
+        assert!(uf.same(5, 9));
     }
 }
