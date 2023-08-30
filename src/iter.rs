e7917946bd733509e8946d2fa522755f4e7e2645
relievedFace <relievedFace@gmail.com>
2023-08-30 20:57:19 +0900

    fix

--- src/iter.rs
+++ src/iter.rs
@@ -6,18 +6,18 @@
 /// use competitive_programming_library::iter::move_into_4_directions;
 ///
 /// let mut result = vec![];
-/// let x = 10i64;
-/// let y = 10i64;
+/// let x = 10usize;
+/// let y = 10usize;
 ///
 /// for (dx, dy) in move_into_4_directions() {
-///     let px = x.wrapping_add(dx);
-///     let py = y.wrapping_add(dy);
+///     let px = x.wrapping_add_signed(dx);
+///     let py = y.wrapping_add_signed(dy);
 ///     result.push((px, py))
 /// }
 ///
 /// assert_eq!(result, vec![(10, 11), (11, 10), (10, 9), (9, 10)]);
 /// ```
-pub fn move_into_4_directions() -> impl Iterator<Item = (i64, i64)> {
+pub fn move_into_4_directions() -> impl Iterator<Item = (isize, isize)> {
     [(0, 1), (1, 0), (0, -1), (-1, 0)].into_iter()
 }
 
@@ -29,18 +29,18 @@ pub fn move_into_4_directions() -> impl Iterator<Item = (i64, i64)> {
 /// use competitive_programming_library::iter::move_into_8_directions;
 ///
 /// let mut result = vec![];
-/// let x = 10i64;
-/// let y = 10i64;
+/// let x = 10usize;
+/// let y = 10usize;
 ///
 /// for (dx, dy) in move_into_8_directions() {
-///     let px = x.wrapping_add(dx);
-///     let py = y.wrapping_add(dy);
+///     let px = x.wrapping_add_signed(dx);
+///     let py = y.wrapping_add_signed(dy);
 ///     result.push((px, py))
 /// }
 ///
 /// assert_eq!(result, vec![(10, 11), (11, 11), (11, 10), (11, 9), (10, 9), (9, 9), (9, 10), (9, 11)]);
 /// ```
-pub fn move_into_8_directions() -> impl Iterator<Item = (i64, i64)> {
+pub fn move_into_8_directions() -> impl Iterator<Item = (isize, isize)> {
     [
         (0, 1),
         (1, 1),
